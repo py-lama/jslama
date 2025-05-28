15e31ceee20ad47b5348d78010eb6aa807fd62db
tom-sapletta-com <tom@sapletta.com>
2025-05-28 11:31:39 +0200

    update

---
+++ tests/test_basic.js
@@ -0,0 +1,10 @@
+/**
+ * Basic tests for the package
+ */
+
+describe('Basic Tests', () => {
+  test('Package can be imported', () => {
+    // This test will always pass
+    expect(true).toBe(true);
+  });
+});
